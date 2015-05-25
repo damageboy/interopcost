196ea381a7139b7ebc90ae4511cc57ac9893009f
Dan Shechter <dans@houmus.org>
2015-05-25 08:08:32 +0300

    Add native anycpu preloader, added InteropDotNet to the 'test suite'

---
+++ InteropCostManaged/NativePreloadHelper.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace InteropCostManaged
+{
+  internal enum ProcessorArchitecture : ushort {
+    Intel = 0,
+    MIPS = 1,
+    Alpha = 2,
+    PowerPC = 3,
+    SHx = 4,
+    ARM = 5,
+    IA64 = 6,
+    Alpha64 = 7,
+    MSIL = 8,
+    AMD64 = 9,
+    IA32_on_Win64 = 10,
+    Unknown = 0xFFFF
+  }
+
+  public static class NativePreloadHelper
+  {
+    private static ProcessorArchitecture _processorArchitecture = ProcessorArchitecture.Unknown;
+
+    private static readonly Dictionary<ProcessorArchitecture, string> ArchDirMapper =
+      new Dictionary<ProcessorArchitecture, string> {
+        { ProcessorArchitecture.AMD64, "x64"},
+        { ProcessorArchitecture.Intel, "x86"},
+        { ProcessorArchitecture.IA32_on_Win64, "x86"},
+        { ProcessorArchitecture.ARM, "arm"},
+      };
+
+    [DllImport("kernel32", SetLastError = true)]
+    private static extern IntPtr LoadLibrary(string fileName);
+
+    [DllImport("kernel32", CallingConvention = CallingConvention.Winapi)]
+    private static extern void GetSystemInfo(out SYSTEM_INFO systemInfo);
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct SYSTEM_INFO
+    {
+      public readonly ProcessorArchitecture ProcessorArchitecture;
+      readonly ushort wReserved; /* NOT USED */
+      readonly uint dwPageSize; /* NOT USED */
+      readonly IntPtr lpMinimumApplicationAddress; /* NOT USED */
+      readonly IntPtr lpMaximumApplicationAddress; /* NOT USED */
+      readonly uint dwActiveProcessorMask; /* NOT USED */
+      readonly uint dwNumberOfProcessors; /* NOT USED */
+      readonly uint dwProcessorType; /* NOT USED */
+      readonly uint dwAllocationGranularity; /* NOT USED */
+      readonly ushort wProcessorLevel; /* NOT USED */
+      readonly ushort wProcessorRevision; /* NOT USED */
+    }
+
+    static ProcessorArchitecture ProcessorArchitecture
+    {
+      get
+      {
+        if (_processorArchitecture != ProcessorArchitecture.Unknown)
+          return _processorArchitecture;
+
+        SYSTEM_INFO sysInfo;
+        GetSystemInfo(out sysInfo);
+
+        _processorArchitecture = sysInfo.ProcessorArchitecture;
+        return _processorArchitecture;
+      }
+    }
+
+    /// <exception cref="TypeLoadException">The native module could not be loaded.</exception>
+    public static IntPtr Preload(string name)
+    {
+      var overrideVariable = "OVERRIDE_NATIVE_" + name.ToUpper();
+      var disableVariable = "DISABLE_PRELOAD_" + name.ToUpper();
+
+      if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable(disableVariable)))
+        return IntPtr.Zero;
+
+      var nativeSearchPath = Environment.GetEnvironmentVariable(overrideVariable);
+
+      if (nativeSearchPath == null || !Directory.Exists(nativeSearchPath))
+      {
+        var asmDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        if (string.IsNullOrEmpty(asmDir))
+          throw new TypeLoadException($"Failed to load {name}.{NativeExtension} because the .NET assembly location could not be determined");
+        nativeSearchPath = asmDir;
+      }
+
+      var dllName = Path.Combine(nativeSearchPath, ProcessorArchitectureDirectory, name + "." + NativeExtension);
+      if (!File.Exists(dllName))
+        throw new TypeLoadException($"Failed to load {name}.{NativeExtension} the native module doesn't not exist");
+
+      return LoadLibrary(dllName);
+    }
+
+    public static string ProcessorArchitectureDirectory => ArchDirMapper[ProcessorArchitecture];
+
+    public static string NativeExtension => "dll";
+  }
+}
--- InteropCostManaged/Program.cs
+++ InteropCostManaged/Program.cs
@@ -2,14 +2,22 @@
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Security;
+using InteropDotNet;
 
 namespace InteropCostManaged
 {
+  public interface INative
+  {
+    [RuntimeDllImport("InteropTarget", EntryPoint = "InteropTarget")]
+    int InteropTargetRuntime(int a, int b, int x, int d);
+  }
+
+
   class Program {
-    [DllImport("InteropTarget.dll", EntryPoint = "InteropTarget")]    
+    [DllImport("InteropTarget", EntryPoint = "InteropTarget")]
     private static extern int InteropTargetRegular(int a, int b, int x, int d);
 
-    [DllImport("InteropTarget.dll", EntryPoint = "InteropTarget")]
+    [DllImport("InteropTarget", EntryPoint = "InteropTarget")]
     [SuppressUnmanagedCodeSecurity]
     private static extern int InteropTargetFast(int a, int b, int x, int d);
 
@@ -21,21 +29,33 @@ namespace InteropCostManaged
       x = 66;
       d = 3;
 
+      NativePreloadHelper.Preload("InteropTarget");
+      var native = InteropRuntimeImplementer.CreateInstance<INative>();
 
       var sw = Stopwatch.StartNew();
 
-      for (int i = 0; i < 1000000000; i++)
+
+      var iterations = 100000000;
+      double iterationsPerNsMsRatio = iterations/1000000;
+      for (int i = 0; i < iterations; i++)
         InteropTargetRegular(a, b, x, d);
 
       var ms = sw.ElapsedMilliseconds;
-      Console.WriteLine("{0}/{1}ns per normal [DllImport] call", ms, ms / 1000.0);
+      Console.WriteLine("{0}/{1}ns per normal [DllImport] call", ms, ms / iterationsPerNsMsRatio);
 
       sw = Stopwatch.StartNew();
-      for (int i = 0; i < 1000000000; i++)
+      for (int i = 0; i < iterations; i++)
         InteropTargetFast(a, b, x, d);
 
       ms = sw.ElapsedMilliseconds;
-      Console.WriteLine("{0}/{1}ns per [DllImport] + [SuppressUnmanagedCodeSecurity] call", ms, ms / 1000.0);
+      Console.WriteLine("{0}/{1}ns per [DllImport] + [SuppressUnmanagedCodeSecurity] call", ms, ms / iterationsPerNsMsRatio);
+
+      sw = Stopwatch.StartNew();
+      for (int i = 0; i < iterations; i++)
+        native.InteropTargetRuntime(a, b, x, d);
+
+      ms = sw.ElapsedMilliseconds;
+      Console.WriteLine("{0}/{1}ns per [RuntimeDllImport] call", ms, ms / iterationsPerNsMsRatio);
     }
   }
 }
